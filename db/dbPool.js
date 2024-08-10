04675ea3fd812eb686326c7d2da052624dd1138b
Kike <kike.mendoza9978@gmail.com>
2024-08-09 21:41:02 -0600

    Add dotenv package

--- db/dbPool.js
+++ db/dbPool.js
@@ -1,4 +1,10 @@
-import { Pool } from "pg"
+import pkg from 'pg';
+import dotenv from "dotenv"
+
+dotenv.config()
+const { Pool } = pkg;
+
+
 const pool = new Pool({ connectionString: process.env.EXTERNAL_DB_URL })
 
 export default pool
